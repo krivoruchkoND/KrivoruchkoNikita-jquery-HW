14219801a79ee81b6b68cf7596c619106947c382
krivoruchkoND <lizey@bk.ru>
2020-04-14 20:07:16 +0300

    basic app architecture init

---
+++ HW2/index.js
@@ -0,0 +1,48 @@
+const state = {
+    products: [
+        { name: 'Product 1', count: 5, price: 1234.56 },
+        { name: 'Product 2', count: 10, price: 7.91 },
+        { name: 'Product 3', count: 7, price: 77.88 }
+    ],
+    sortBy: 'NAME',
+    sortOrder: 'DESC' //ASC
+};
+
+const toggleSortBy = () => {
+
+};
+
+const toggleSortOrder = () => {
+
+};
+
+const searchHandler = () => {
+
+};
+
+const addNewHandler = () => {
+
+};
+
+const editHandler = () => {
+
+};
+
+const deleteHandler = () => {
+
+};
+
+const renderTableHead = () => {
+    
+};
+
+const renderTableBody = () => {
+    
+};
+
+const render = () => {
+    renderTableHead();
+    renderTableBody();
+};
+
+render();
