fa3bda7c2f68fe92bf3ff894f88ae6eac4912904
krivoruchkoND <lizey@bk.ru>
2020-04-14 21:16:55 +0300

    renderTableHead done whithout proper styles

--- HW2/index.js
+++ HW2/index.js
@@ -18,6 +18,19 @@ const toggleSortOrder = () => {
     state.sortOrder = sortOrder === 'DESC' ? 'ASC' : 'DESC';
 };
 
+const toggleSort = (event) => {
+    const { type } = event.data;
+    const { sortBy } = state;
+
+    if (type === sortBy) {
+        toggleSortOrder();
+    } else {
+        state.sortOrder = 'DESC';
+        toggleSortBy();
+    };
+    render();
+}
+
 const searchHandler = () => {
 
 };
@@ -35,7 +48,40 @@ const deleteHandler = () => {
 };
 
 const renderTableHead = () => {
-    
+    const tableHead = $('#tableHead')
+    const { sortBy, sortOrder } = state;
+
+    const sortIcon = sortOrder === 'ASC' ? 'up-arrow' : 'down-arrow';
+    const sortIcontEl = $('<div>', { class: sortIcon });
+
+    const nameEl = $('<th>').append(
+        $('<a>', { href: '#' })
+            .text('Name')
+            .click({ type: 'NAME' }, toggleSort)
+    );
+
+    const priceEl = $('<th>').append(
+        $('<a>', { href: '#' })
+            .text('Price')
+            .click({ type: 'PRICE' }, toggleSort)
+    );
+    sortBy === 'NAME' ? nameEl.append(sortIcontEl) : priceEl.append(sortIcontEl);
+
+    const actionsEl = $('<th>').append(
+        $('<button>', { class: "btn btn-primary" })
+            .text('Edit')
+            .click(editHandler),
+        $('<button>', { class: "btn btn-primary" })
+            .text('Delete')
+            .click(deleteHandler),
+    );
+
+    $(tableHead).empty();
+    $('<tr>').append(
+        $(nameEl),
+        $(priceEl),
+        $(actionsEl)
+    ).appendTo(tableHead);
 };
 
 const renderTableBody = () => {
