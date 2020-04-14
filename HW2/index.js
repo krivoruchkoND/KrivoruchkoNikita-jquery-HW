513d9ed6db1160cb5dc20d7937b59e479133d090
krivoruchkoND <lizey@bk.ru>
2020-04-14 20:12:05 +0300

    togglers done

--- HW2/index.js
+++ HW2/index.js
@@ -2,18 +2,20 @@ const state = {
     products: [
         { name: 'Product 1', count: 5, price: 1234.56 },
         { name: 'Product 2', count: 10, price: 7.91 },
-        { name: 'Product 3', count: 7, price: 77.88 }
+        { name: 'Product 3', count: 7, price: 77.88 },
     ],
-    sortBy: 'NAME',
-    sortOrder: 'DESC' //ASC
+    sortBy: 'NAME', //PRICE
+    sortOrder: 'DESC', //ASC
 };
 
 const toggleSortBy = () => {
-
+    const { sortBy } = state;
+    state.sortBy = sortBy === 'NAME' ? 'PRICE' : 'NAME';
 };
 
 const toggleSortOrder = () => {
-
+    const { sortOrder } = state;
+    state.sortOrder = sortOrder === 'DESC' ? 'ASC' : 'DESC';
 };
 
 const searchHandler = () => {
